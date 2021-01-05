f3796d808a2f0dc2a7aaf6e5888fd91cd9da77ee
racycars <racycars@gmail.com>
2021-01-05 21:22:05 +1300

    Add Final line for best day of week

--- Part_D/src/PartD.java
+++ Part_D/src/PartD.java
@@ -6,7 +6,7 @@ public class PartD {
 		System.out.println("Hi, my name is daniel");
 		System.out.println("My favourite colour is blue");
 		System.out.println("My favourite mode of transportation is car");
-	
+		System.out.println("The best day of the week is friday");
 		
 		
 		
